f9bf32ef61e945904ec8e9acf1656dae5e057919
今村友哉 <imamuratomoya@tomoyanoMacBook-Pro.local>
2021-07-28 00:20:58 +0900

    push

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+// expo install react-native-webview
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,View} from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView source={{uri: 'https://code-database.com/'}}/>
+      {/* <WebView source={{uri: 'https://www.youtube.com/'}}/> */}
     </View>
   );
 }
@@ -15,7 +16,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:"10%",
   },
 });
